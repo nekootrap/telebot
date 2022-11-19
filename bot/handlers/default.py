e8b6a1462f8033ed2ecd9e309b07c09a9b788439
polina <polatihonova933@gmail.com>
2022-11-19 21:46:10 +0300

    addcommands

--- bot/handlers/default.py
+++ bot/handlers/default.py
@@ -1,4 +1,8 @@
+import datetime
+from urllib.parse import quote
+from urllib import request
 from telebot import types
+from random import randint
 
 from starters.add_bot import bot
 
@@ -10,3 +14,102 @@ def send_help(message: types.Message):
 
 def exo(message: types.Message):
     bot.reply_to(message, message.text)
+
+
+def bread(message: types.Message):
+    msg_text = 'пизда'
+    bot.reply_to(message, msg_text)
+
+
+def season(message: types.Message):
+    seasons = {
+        (12, 1, 2): 'зима',
+        (3, 4, 5): 'весна',
+        (6, 7, 8): 'лето',
+        (9, 10, 11): 'осень',
+        }
+
+    data = message.text.split()
+
+    if len(data) == 2:
+        data = data[1]
+
+        if data.isdigit():
+            data = int(data)
+
+            key = [el for el in seasons.keys() if data in el]
+            if len(key):
+                key = key[0]
+            else:
+                mess = 'ты тупой?'
+                bot.reply_to(message, mess)
+                return
+
+            answer = seasons[key]
+
+            bot.reply_to(message, answer)
+
+        else:
+            mess = 'ты тупой?'
+            bot.reply_to(message, mess)
+
+    elif len(data) == 1:
+        month = datetime.datetime.today().month
+
+        key = [el for el in seasons.keys() if month in el]
+        key = key[0]
+
+        answer = seasons[key]
+
+        bot.reply_to(message, answer)
+
+    else:
+        mess = 'ты тупой?'
+        bot.reply_to(message, mess)
+
+
+def weather(message: types.Message):
+    data = message.text.split()
+    
+    #хачу трахаца
+
+    match data:
+        case [command]:
+            city = 'Йошкар-Ола'
+
+        case [command, *city]:
+            city = ' '.join(city)
+
+    try:
+        result = request.urlopen(f'https://wttr.in/{quote(city)}_pq_lang=ru.png')
+
+        with open('./imgs/wttr.png', 'wb') as file_img:
+            file_img.write(result.read())
+
+        image = open('./imgs/wttr.png', 'rb')
+
+        bot.send_photo(
+            message.chat.id,
+            photo=image,
+            caption=f'Погода в городе <u><b>{city}</b></u>',
+            parse_mode='HTML'
+            #я тебя люблю
+        )
+
+    except Exception:
+        bot.reply_to(message, 'произошли технические шоколадки :<')
+
+def guess_the_number(message: types.Message):
+    intt = randint(1, 11)
+    
+    user_number = message.text
+    
+    if user_number.isdigit():
+        if user_number == str(intt):
+            return 'верно!'
+        else:
+            return 'неверно('
+        
+    else:
+        return 'это не число!'
+    
--- bot/starters/register_handlers.py
+++ bot/starters/register_handlers.py
@@ -1,15 +1,35 @@
-from handlers.default import send_help, exo
+from handlers.default import send_help, exo, bread, season, weather, guess_the_number
 
 
 def register_all_handlers(bot):
     bot.register_message_handler(
         send_help,
-        commands = ['start', 'help']
+        commands=['start', 'help']
+    )
+    
+    bot.register_message_handler(
+        season,
+        commands=['сезон']
     )
 
     bot.register_message_handler(
-        exo,
-        func=lambda m: True
+        weather,
+        commands=['w', 'погода', 'п']
     )
+
+    bot.register_message_handler(
+        bread,
+        func=lambda m: m.text in ('да', 'Да')
+    )
+    
+    bot.register_message_handler(
+        guess_the_number,
+        func=lambda x: True
+    )
+    # bot.register_message_handler(
+    #     exo,
+    #     func=lambda m: True
+        
+    # )
 
     return bot
